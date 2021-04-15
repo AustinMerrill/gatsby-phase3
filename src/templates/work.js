8b6bbce68071b924085807aa84377e62bd59e36c
Austin Merrill <austinmerrill@Austins-MBP.lan>
2021-04-15 10:56:48 -0500

    Updated work page

--- src/templates/work.js
+++ src/templates/work.js
@@ -6,27 +6,42 @@ const WorkTemplate = ({ data }) => {
   console.log("this is for the tempalte", data)
   return (
     <Layout>
-      <h1 className="text-6xl font-semibold text-gray-800">
-        {data.sanityWorkPage.title}
-      </h1>
-      <h2 className="text-3xl font-semibold text-gray-600">
-        {data.sanityWorkPage.description}
-      </h2>
-      <p>
-        <a href={data.sanityWorkPage.github} className="text-blue-400">
-          Github Repo
-        </a>
-      </p>
-      <p>
-        {" "}
-        <a href={data.sanityWorkPage.liveSite} className="text-blue-400">
-          Live Site
-        </a>
-      </p>
-      <img
-        src={data.sanityWorkPage.image.asset.fluid.src}
-        alt="something"
-      ></img>
+      <div className="container mx-auto  flex flex-col justify-center align-center">
+        <div className="flex flex-col justify-center align-center text-center mb-4">
+          <h1 className="text-6xl font-semibold text-gray-800">
+            {data.sanityWorkPage.title}
+          </h1>
+          <h2 className="text-3xl font-semibold text-gray-600">
+            {data.sanityWorkPage.description}
+          </h2>
+        </div>
+        <div className="flex justify-center align-center">
+          <img
+            className="h-auto md:w-1/2 w-3/4"
+            src={data.sanityWorkPage.image.asset.fluid.src}
+            alt="something"
+          ></img>
+        </div>
+        <div className="flex align-center justify-center text-center w-full  mt-8">
+          <p className="mr-6">
+            <a
+              href={data.sanityWorkPage.github}
+              className="text-gray-200 bg-gray-800 py-2 px-4 rounded"
+            >
+              Github Repo
+            </a>
+          </p>
+          <p className="">
+            {" "}
+            <a
+              href={data.sanityWorkPage.liveSite}
+              className="text-gray-200 bg-gray-800  py-2 px-8 rounded"
+            >
+              Live Site
+            </a>
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
